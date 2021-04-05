8166591d229a7124d66a20ec1d8f731a6f609a82
Andrej Germič <andrej.germic@gmail.com>
2021-04-05 13:33:06 +0200

    Fixed visual plan layout

--- web-app/src/components/Plans/Visual.tsx
+++ web-app/src/components/Plans/Visual.tsx
@@ -4,7 +4,7 @@ import { connect } from "react-redux";
 import { ReduxStoreT } from "../../redux/reduxStore";
 import { getMaxMin } from "../../utilities/getMaxMin";
 import { editValuesT } from "../../redux/Plans/Reducer";
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import { plansToImms } from "../../utilities/planToImms";
 import { switchPerMolds } from "../../types/globalTypes";
 
@@ -27,8 +27,21 @@ const colors = ["LawnGreen", "aqua", "DarkOrchid", "Gold", "Chartreuse", "Red"];
 const Visual: FC<VisualProps> = ({ plans }) => {
   const { min } = getMaxMin(plans, "time");
   const immPlans = plansToImms(plans);
-  const startTime = min - (min % (8 * 60 * 60 * 1000)) + 4 * 60 * 60 * 1000;
-  const endTime = startTime + 8 * 60 * 60 * 1000;
+  const time = new Date(min);
+  let startTime = min;
+
+  for (const t of [6, 14, 22]) {
+    time.setHours(t);
+    if (time.getTime() <= min) {
+      startTime = time.getTime();
+    } else {
+      console.log(time.getTime(), "is not bigger than", min);
+    }
+
+    console.log(new Date(startTime).toLocaleTimeString());
+  }
+
+  const endTime = startTime + 7.5 * 60 * 60 * 1000;
   return (
     <Flex h="100%" w={`${Object.keys(immPlans).length * 75}px`}>
       {Object.keys(immPlans)
@@ -53,32 +66,35 @@ const Visual: FC<VisualProps> = ({ plans }) => {
               >
                 {imm.slice(3)}
               </Heading>
-              {immPlans[imm].map((swtch: switchPerMolds, index) => {
-                if (swtch.time > endTime) {
-                  return null;
-                }
-                return (
-                  <Flex
-                    position="absolute"
-                    top={`${
-                      ((swtch.time - startTime) / (30 * 60 * 1000)) * (100 / 18)
-                    }%`}
-                    left="0"
-                    right="0"
-                    h={`${100 / 9}%`}
-                    textAlign="center"
-                    fontSize="18px"
-                    key={index}
-                    direction="column"
-                    bg={colors[index]}
-                  >
-                    <Text fontWeight="700">
-                      {new Date(swtch.time).toLocaleTimeString().slice(0, 5)}
-                    </Text>
-                    <Text fontSize="30px">{swtch.nextForm}</Text>
-                  </Flex>
-                );
-              })}
+              <Box position="relative" h="100%" overflow="hidden">
+                {immPlans[imm].map((swtch: switchPerMolds, index) => {
+                  if (swtch.time > endTime) {
+                    return null;
+                  }
+                  return (
+                    <Flex
+                      position="absolute"
+                      top={`${
+                        ((swtch.time - startTime) / (30 * 60 * 1000)) *
+                        (100 / 17)
+                      }%`}
+                      left="0"
+                      right="0"
+                      h={`${100 / 8.5}%`}
+                      textAlign="center"
+                      fontSize="18px"
+                      key={index}
+                      direction="column"
+                      bg={colors[index]}
+                    >
+                      <Text fontWeight="700">
+                        {new Date(swtch.time).toLocaleTimeString().slice(0, 5)}
+                      </Text>
+                      <Text fontSize="30px">{swtch.nextForm}</Text>
+                    </Flex>
+                  );
+                })}
+              </Box>
             </Flex>
           );
         })}
